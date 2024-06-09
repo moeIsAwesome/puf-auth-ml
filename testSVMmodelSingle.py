6fe8fb9a8c7de8e9438ffaeb8fe814d15d4f23fc
Moe <musavimoe@gmail.com>
2024-06-09 23:26:43 +0200

    add test svm batch

--- testSVMmodelSingle.py
+++ testSVMmodelSingle.py
@@ -1,16 +1,11 @@
-# predict_model.py
-
 import joblib
 import numpy as np
 
 # Function to read .bin file and convert to flat array of bytes
-
-
 def read_bin_file(file_path):
     with open(file_path, 'rb') as file:  # Open the file in binary mode
         byte_data = file.read()
         return np.frombuffer(byte_data, dtype=np.uint8)
-
 
 # Load the compressed model
 model_filename = 'svm_model_compressed.pkl'
@@ -18,8 +13,6 @@ loaded_model = joblib.load(model_filename)
 print(f"Loaded model from {model_filename}")
 
 # Function to make predictions on new PUF responses with confidence scores
-
-
 def predict_puf_response(model, file_path):
     # Read the binary file and convert to a flat array of bytes
     response = read_bin_file(file_path)
@@ -32,12 +25,9 @@ def predict_puf_response(model, file_path):
     puf_labels = {0: 'RPi1', 1: 'RPi2', 2: 'RPi3'}
     predicted_label = puf_labels[prediction[0]]
     confidence_score = prediction_proba[0][prediction[0]]
-    print(
-        f"Predicted Label: {predicted_label}, Confidence Score: {confidence_score:.2f}")
+    print(f"Predicted Label: {predicted_label}, Confidence Score: {confidence_score:.2f}")
     return predicted_label, confidence_score
 
-
 # Example usage
-new_puf_file = './dataset/data/CorrTest/30-percent-corrupted/rpi1_0_corrBot.bin'
+new_puf_file = './dataset/data/RPi1Dump/rpi1_0_corrMid.bin'
 predicted_puf, confidence = predict_puf_response(loaded_model, new_puf_file)
-
