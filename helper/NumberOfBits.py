0e559709682133b5f777c363c8d89ea834fb3d00
Moe <musavimoe@gmail.com>
2024-06-18 15:03:14 +0200

    add number of bits calculator

---
+++ helper/NumberOfBits.py
@@ -0,0 +1,9 @@
+def count_bits(file_path):
+    with open(file_path, 'rb') as file:
+        binary_data = file.read()
+    bit_count = len(binary_data) * 8
+    return bit_count
+
+file_path = '../data//RPi1Dump/rpi1_0.bin'
+bits = count_bits(file_path)
+print(f'The file contains {bits} bits.')
