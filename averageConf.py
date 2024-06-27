f8d8e6ca6900bde33c31bd3a96688d3b9cbd3080
Moe <musavimoe@gmail.com>
2024-06-27 17:11:56 +0200

    add augmented

--- averageConf.py
+++ averageConf.py
@@ -9,7 +9,7 @@ def read_bin_file(file_path):
         return np.frombuffer(byte_data, dtype=np.uint8)
 
 # Load the compressed model
-model_filename = './reports/trained_with_30p_full_length/model-trained-with-all-30-.pkl'
+model_filename = './reports/trained_with_augmented_full_length/model-trained-on-augmented.pkl'
 loaded_model = joblib.load(model_filename)
 print(f"Loaded model from {model_filename}")
 
